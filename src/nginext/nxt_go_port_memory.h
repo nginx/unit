e06872e2f281c157b3f7478fad9469f1ec0257ba
Max Romanov <max.romanov@nginx.com>
2017-08-30 14:34:31 -0700

    Avoid nxt_port_mmap_header_t redefinition warning.

--- src/nginext/nxt_go_port_memory.h
+++ src/nginext/nxt_go_port_memory.h
@@ -11,14 +11,12 @@
 #include <nxt_main.h>
 #include <nxt_port_memory_int.h>
 
-typedef struct nxt_port_mmap_header_s  nxt_port_mmap_header_t;
-
 #ifndef _NXT_GO_PROCESS_T_DEFINED_
 #define _NXT_GO_PROCESS_T_DEFINED_
 typedef struct nxt_go_process_s nxt_go_process_t;
 #endif
 
-nxt_port_mmap_header_t *
+struct nxt_port_mmap_header_s *
 nxt_go_port_mmap_get(nxt_go_process_t *process, nxt_port_id_t port_id,
     nxt_chunk_id_t *c);
 
