dc50773e518f6e68109384cb37c91985dbc3fe63
Valentin Bartenev <vbart@nginx.com>
2018-02-12 16:58:38 +0300

    Version bump.

--- src/nxt_main.h
+++ src/nxt_main.h
@@ -11,8 +11,8 @@
 #include <nxt_auto_config.h>
 
 
-#define NXT_VERSION                  "0.6"
-#define NXT_VERNUM                   6
+#define NXT_VERSION                  "0.7"
+#define NXT_VERNUM                   7
 
 
 typedef struct nxt_port_s            nxt_port_t;
