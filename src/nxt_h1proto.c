b5d4fc939edca5ea5858b66d5fcf789b660df243
Igor Sysoev <igor@sysoev.ru>
2018-09-28 17:32:02 +0300

    Disabled chunked transfer encoding for 204 responses.

--- src/nxt_h1proto.c
+++ src/nxt_h1proto.c
@@ -967,7 +967,8 @@ nxt_h1p_request_header_send(nxt_task_t *task, nxt_http_request_t *r)
     http11 = (h1p->parser.version.s.minor != '0');
 
     if (r->resp.content_length == NULL || r->resp.content_length->skip) {
-        if (http11) {
+
+        if (http11 && n != NXT_HTTP_NO_CONTENT) {
             h1p->chunked = 1;
             size += nxt_length(chunked);
             /* Trailing CRLF will be added by the first chunk header. */
