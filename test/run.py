23a05d6ca8d2a971af09809fed8a9f8a85413c91
Tiago de Bem Natel de Moura <t.nateldemoura@f5.com>
2019-08-12 12:24:14 +0100

    add env var to test specific pattern
    Signed-off-by: Tiago de Bem Natel de Moura <t.nateldemoura@f5.com>

--- test/run.py
+++ test/run.py
@@ -7,8 +7,9 @@ import os
 loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
+testpattern = os.getenv("UNIT_TEST", "test*.py")
 this_dir = os.path.dirname(__file__)
-tests = loader.discover(start_dir=this_dir)
+tests = loader.discover(start_dir=this_dir, pattern=testpattern)
 suite.addTests(tests)
 
 runner = unittest.TextTestRunner(verbosity=3)
