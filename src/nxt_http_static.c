5c50329f8edcbf3b56d9ffbc207eae64ca72e0fb
Valentin Bartenev <vbart@nginx.com>
2019-09-20 14:31:19 +0300

    Fixed segfault if an inappropriate file system object is requested.
    Found by Coverity (CID 349483).

--- src/nxt_http_static.c
+++ src/nxt_http_static.c
@@ -209,7 +209,6 @@ nxt_http_static_handler(nxt_task_t *task, nxt_http_request_t *r,
         /* Not a file. */
 
         nxt_file_close(task, f);
-        f = NULL;
 
         if (nxt_slow_path(!nxt_is_dir(&fi))) {
             nxt_log(task, NXT_LOG_ERR, "\"%FN\" is not a regular file",
@@ -217,6 +216,8 @@ nxt_http_static_handler(nxt_task_t *task, nxt_http_request_t *r,
             nxt_http_request_error(task, r, NXT_HTTP_NOT_FOUND);
             return NULL;
         }
+
+        f = NULL;
 
         r->status = NXT_HTTP_MOVED_PERMANENTLY;
         r->resp.content_length_n = 0;
