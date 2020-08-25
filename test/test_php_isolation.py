d1bdaf98ba15cdba62e1f2ecb83c37e08728682c
Tiago Natel de Moura <t.nateldemoura@f5.com>
2020-08-25 15:27:51 +0100

    Tests: disable of language_deps.

--- test/test_php_isolation.py
+++ test/test_php_isolation.py
@@ -107,5 +107,74 @@ class TestPHPIsolation(TestApplicationPHP):
         self.assertIn('unit', extensions, 'unit in extensions list')
 
 
+    def test_php_isolation_rootfs_no_language_libs(self):
+        isolation_features = self.available['features']['isolation'].keys()
+
+        if not self.is_su:
+            if 'user' not in isolation_features:
+                print('requires unprivileged userns or root')
+                raise unittest.SkipTest()
+
+            if not 'unprivileged_userns_clone' in isolation_features:
+                print('requires unprivileged userns or root')
+                raise unittest.SkipTest()
+
+            if 'mnt' not in isolation_features:
+                print('requires mnt ns')
+                raise unittest.SkipTest()
+
+        isolation = {
+            'rootfs': self.current_dir,
+            'automount': {'language_deps': False},
+            'namespaces': {
+                'credential': not self.is_su,
+                'mount': not self.is_su,
+            },
+        }
+
+        self.load('list-extensions', isolation=isolation)
+
+        self.assertIn(
+            'success',
+            self.conf(
+                '"/php/list-extensions"', 'applications/list-extensions/root'
+            ),
+        )
+
+        self.assertIn(
+            'success',
+            self.conf(
+                {'file': '/php/list-extensions/php.ini'},
+                'applications/list-extensions/options',
+            ),
+        )
+
+        self.assertIn(
+            'success',
+            self.conf(
+                '"/php/list-extensions"',
+                'applications/list-extensions/working_directory',
+            ),
+        )
+
+        extensions = self.getjson()['body']
+
+        self.assertIn('unit', extensions, 'unit in extensions list')
+        self.assertNotIn('json', extensions, 'json not in extensions list')
+
+        self.assertIn(
+            'success',
+            self.conf(
+                {'language_deps': True},
+                'applications/list-extensions/isolation/automount',
+            ),
+        )
+
+        extensions = self.getjson()['body']
+
+        self.assertIn('unit', extensions, 'unit in extensions list 2')
+        self.assertIn('json', extensions, 'json in extensions list 2')
+
+
 if __name__ == '__main__':
     TestPHPIsolation.main()
