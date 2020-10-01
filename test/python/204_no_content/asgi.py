12f225a43acc6b5086b08c3d7df6f6ac2322efa1
Max Romanov <max.romanov@nginx.com>
2020-10-01 23:55:43 +0300

    Tests: added ASGI HTTP applications.

---
+++ test/python/204_no_content/asgi.py
@@ -0,0 +1,8 @@
+async def application(scope, receive, send):
+    assert scope['type'] == 'http'
+
+    await send({
+        'type': 'http.response.start',
+        'status': 204,
+        'headers': [],
+    })
