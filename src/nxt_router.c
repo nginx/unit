73ac0496feb38ef2098163847206d304cf2c9a73
Max Romanov <max.romanov@nginx.com>
2021-03-02 18:31:03 +0300

    Fixing warnings on Solaris.
    pthread_t on Solaris is an integer type with size not equal to pointer size.
    To avoid warnings, type casts to and from pointer needs to be done via
    uintptr_t type.
    
    This change originally proposed by Juraj Lutter <juraj@lutter.sk>.

--- src/nxt_router.c
+++ src/nxt_router.c
@@ -3726,7 +3726,7 @@ nxt_router_thread_exit_handler(nxt_task_t *task, void *obj, void *data)
     nxt_event_engine_t   *engine;
     nxt_thread_handle_t  handle;
 
-    handle = (nxt_thread_handle_t) obj;
+    handle = (nxt_thread_handle_t) (uintptr_t) obj;
     link = data;
 
     nxt_thread_wait(handle);
