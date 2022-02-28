6e36584a2e9bad0e7c4f96d68c527992deadc650
Alejandro Colomar <alx.manpages@gmail.com>
2022-02-28 12:12:30 +0100

    Supporting UNIX sockets in address matching.
    This closes #645 issue on GitHub.
    
    (Also moved a changelog line that was misplaced in a previous commit.)

--- src/nxt_http_route_addr.c
+++ src/nxt_http_route_addr.c
@@ -41,6 +41,16 @@ nxt_http_route_addr_pattern_parse(nxt_mp_t *mp,
         base->negative = 0;
     }
 
+    if (nxt_str_eq(&addr, "unix", 4)) {
+#if (NXT_HAVE_UNIX_DOMAIN)
+        base->addr_family = AF_UNIX;
+
+        return NXT_OK;
+#else
+        return NXT_ADDR_PATTERN_NO_UNIX_ERROR;
+#endif
+    }
+
     if (nxt_slow_path(addr.length < 2)) {
         return NXT_ADDR_PATTERN_LENGTH_ERROR;
     }
