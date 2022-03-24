8d1df4121295f929c7b2b5ea974f223ca25e50d5
echolimazulu <26638058+echolimazulu@users.noreply.github.com>
2022-03-25 00:27:26 +0400

    Array: Multiple fixes, functionality enhancements.
    fixes:
    - removed incorrect array initialization (nxt_array_init) with a pointer to a non-existent set of elements;
    - fixed the size of the allocated memory for storing the array structure (nxt_array_t) - waste of memory;
    - fixed initialization of a set of elements (elts), pointer, memory size;
    - fixed the formula for calculating the required amount of memory (nalloc) allocated for the created array;
    - eliminated the possibility of early deletion of an existing pointer to a set of elements (elts) at the time of memory reallocation;
    
    improvements:
    - added descriptions of functions;
    - added a function to get a pointer by the index of an element in an array.

--- src/nxt_array.h
+++ src/nxt_array.h
@@ -1,5 +1,6 @@
 
 /*
+ * Copyright (C) Evgenii Sokolov
  * Copyright (C) Igor Sysoev
  * Copyright (C) NGINX, Inc.
  */
@@ -9,34 +10,32 @@
 
 
 typedef struct {
-    void      *elts;
-    /* nelts has uint32_t type because it is used most often. */
-    uint32_t  nelts;
-    uint16_t  size;
-    uint16_t  nalloc;
-    nxt_mp_t  *mem_pool;
+    void                *elts;
+    nxt_uint_t          nelts;
+    nxt_uint_t          size;
+    nxt_uint_t          nalloc;
+    nxt_mp_t            *mem_pool;
 } nxt_array_t;
 
 
-nxt_inline void
-nxt_array_init(nxt_array_t *array, nxt_mp_t *mp, size_t size)
-{
-    array->elts = nxt_pointer_to(array, sizeof(nxt_array_t));
-    array->size = size;
-    array->mem_pool = mp;
-}
-
 NXT_EXPORT nxt_array_t *nxt_array_create(nxt_mp_t *mp, nxt_uint_t n,
-    size_t size);
+                                         size_t size);
 NXT_EXPORT void nxt_array_destroy(nxt_array_t *array);
 NXT_EXPORT void *nxt_array_add(nxt_array_t *array);
 NXT_EXPORT void *nxt_array_zero_add(nxt_array_t *array);
-NXT_EXPORT void nxt_array_remove(nxt_array_t *array, void *elt);
-NXT_EXPORT nxt_array_t *nxt_array_copy(nxt_mp_t *mp, nxt_array_t *dst,
-    nxt_array_t *src);
+NXT_EXPORT void nxt_array_del(nxt_array_t *array, void *elt);
+NXT_EXPORT void nxt_array_del_last(nxt_array_t *array);
+NXT_EXPORT nxt_array_t *nxt_array_copy(nxt_mp_t *mp,
+                                       nxt_array_t *array);
+
 
 #define                                                                       \
-nxt_array_last(array)                                                         \
+nxt_array_pointer_to_index(array, index)                                      \
+    nxt_pointer_to((array)->elts, (array)->size * (index))
+
+
+#define                                                                       \
+nxt_array_pointer_to_last(array)                                              \
     nxt_pointer_to((array)->elts, (array)->size * ((array)->nelts - 1))
 
 
@@ -48,14 +47,6 @@ nxt_array_reset(array)                                                        \
 #define                                                                       \
 nxt_array_is_empty(array)                                                     \
     ((array)->nelts == 0)
-
-
-nxt_inline void *
-nxt_array_remove_last(nxt_array_t *array)
-{
-    array->nelts--;
-    return nxt_pointer_to(array->elts, array->size * array->nelts);
-}
 
 
 #endif /* _NXT_ARRAY_H_INCLUDED_ */
