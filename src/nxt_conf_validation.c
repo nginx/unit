dad7ef9a12f2cf16f2be011f167fc19b011ffe03
Andrew Clayton <a.clayton@nginx.com>
2022-05-04 16:13:19 +0100

    Configuration: made large_header_buffer_size a valid setting.
    @JanMikes and @tagur87 on GitHub both reported issues with long URLs
    that were exceeding the 8192 byte large_header_buffer_size setting,
    which resulted in a HTTP 431 error (Request Header Fields Too Large).
    
    This can be resolved in the code by updating the following line in
    src/nxt_router.c::nxt_router_conf_create()
    
        skcf->large_header_buffer_size = 8192;
    
    However, requiring users to modify unit and install custom versions is
    less than ideal. We could increase the value, but to what?
    
    This commit takes the option of allowing the user to set this option in
    their config by making large_header_buffer_size a valid configuration
    setting.
    
    large_header_buffer_size is already set by the configuration system in
    nxt_router.c it just isn't set as a valid config option in
    nxt_conf_validation.c
    
    With this change users can set this option in their config if required
    by the following
    
        "settings": {
            "http": {
                "large_header_buffer_size": 16384
            }
        },
    
    It retains its default value of 8192 bytes if this is not set.
    
    With this commit, without the above setting or too low a value, with a
    long URL you get a 431 error. With the above setting set to a large
    enough value, the request is successful.
    
    NOTE: This setting really determines the maximum size of any single
          header _value_. Also, unit will try and place multiple values
          into a buffer _if_ they fully fit.
    
    NOTE: This is being released as undocumented and subject to change as it
          exposes internal workings of unit.
    
    Closes: <https://github.com/nginx/unit/issues/521>
    Signed-off-by: Andrew Clayton <a.clayton@nginx.com>

--- src/nxt_conf_validation.c
+++ src/nxt_conf_validation.c
@@ -315,6 +315,9 @@ static nxt_conf_vldt_object_t  nxt_conf_vldt_http_members[] = {
         .name       = nxt_string("idle_timeout"),
         .type       = NXT_CONF_VLDT_INTEGER,
     }, {
+        .name       = nxt_string("large_header_buffer_size"),
+        .type       = NXT_CONF_VLDT_INTEGER,
+    }, {
         .name       = nxt_string("body_buffer_size"),
         .type       = NXT_CONF_VLDT_INTEGER,
     }, {
