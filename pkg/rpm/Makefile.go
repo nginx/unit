237ddbe1770485eab0723aac3a5dca92e1a0c5d9
Konstantin Pavlov <thresh@nginx.com>
2022-05-26 10:52:58 +0400

    Packaging: Go: use GO111MODULE=auto in build instructions.

--- pkg/rpm/Makefile.go
+++ pkg/rpm/Makefile.go
@@ -65,7 +65,7 @@ The $(MODULE_SUMMARY_go) has been installed.
 
 To check the sample app, run these commands:
 
- GOPATH=%{gopath} go build -o /tmp/go-app /usr/share/doc/unit-go/examples/go-app/let-my-people.go
+ GOPATH=%{gopath} GO111MODULE=auto go build -o /tmp/go-app /usr/share/doc/unit-go/examples/go-app/let-my-people.go
  sudo service unit start
  cd /usr/share/doc/%{name}/examples
  sudo curl -X PUT --data-binary @unit.config --unix-socket /var/run/unit/control.sock http://localhost/config
