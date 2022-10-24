02dbc1c11ebd28f2140c0fb895e610971313789a
javad mnjd <javad.mnjd@hotmail.com>
2022-10-25 03:07:52 +0330

    feat: improve docker-entrypoint.sh

--- pkg/docker/docker-entrypoint.sh
+++ pkg/docker/docker-entrypoint.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
 curl_put()
 {
-    RET=`/usr/bin/curl -s -w '%{http_code}' -X PUT --data-binary @$1 --unix-socket /var/run/control.unit.sock http://localhost/$2`
-    RET_BODY=${RET::-3}
+    RET=$(/usr/bin/curl -s -w '%{http_code}' -X PUT --data-binary @$1 --unix-socket /var/run/control.unit.sock http://localhost/$2)
+    RET_BODY=$(printf '%s' "$RET" | cut -c 1)
     RET_STATUS=$(echo $RET | /usr/bin/tail -c 4)
     if [ "$RET_STATUS" -ne "200" ]; then
         echo "$0: Error: HTTP response status code is '$RET_STATUS'"
@@ -18,7 +18,7 @@ curl_put()
     return 0
 }
 
-if [ "$1" = "unitd" -o "$1" = "unitd-debug" ]; then
+if [ "$1" = "unitd" ] || [ "$1" = "unitd-debug" ]; then
     if /usr/bin/find "/var/lib/unit/" -mindepth 1 -print -quit 2>/dev/null | /bin/grep -q .; then
         echo "$0: /var/lib/unit/ is not empty, skipping initial configuration..."
     else
@@ -32,30 +32,30 @@ if [ "$1" = "unitd" -o "$1" = "unitd-debug" ]; then
             /usr/bin/curl -s -X GET --unix-socket /var/run/control.unit.sock http://localhost/
 
             echo "$0: Looking for certificate bundles in /docker-entrypoint.d/..."
-            for f in $(/usr/bin/find /docker-entrypoint.d/ -type f -name "*.pem"); do
+            for f in $(/usr/bin/find /docker-entrypoint.d/ \( -type l -o -type f \) -name "*.pem"); do
                 echo "$0: Uploading certificates bundle: $f"
                 curl_put $f "certificates/$(basename $f .pem)"
             done
 
             echo "$0: Looking for configuration snippets in /docker-entrypoint.d/..."
-            for f in $(/usr/bin/find /docker-entrypoint.d/ -type f -name "*.json"); do
+            for f in $(/usr/bin/find /docker-entrypoint.d/ \( -type l -o -type f \) -name "*.json"); do
                 echo "$0: Applying configuration $f";
                 curl_put $f "config"
             done
 
             echo "$0: Looking for shell scripts in /docker-entrypoint.d/..."
-            for f in $(/usr/bin/find /docker-entrypoint.d/ -type f -name "*.sh"); do
+            for f in $(/usr/bin/find /docker-entrypoint.d/ \( -type l -o -type f \) -name "*.sh"); do
                 echo "$0: Launching $f";
                 "$f"
             done
 
             # warn on filetypes we don't know what to do with
-            for f in $(/usr/bin/find /docker-entrypoint.d/ -type f -not -name "*.sh" -not -name "*.json" -not -name "*.pem"); do
+            for f in $(/usr/bin/find /docker-entrypoint.d/ \( -type l -o -type f \) -not -name "*.sh" -not -name "*.json" -not -name "*.pem"); do
                 echo "$0: Ignoring $f";
             done
 
             echo "$0: Stopping Unit daemon after initial configuration..."
-            kill -TERM `/bin/cat /var/run/unit.pid`
+            kill -TERM $(/bin/cat /var/run/unit.pid)
 
             while [ -S /var/run/control.unit.sock ]; do echo "$0: Waiting for control socket to be removed..."; /bin/sleep 0.1; done
 
