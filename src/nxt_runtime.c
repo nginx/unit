nginx/unit

commit 5a37171f733fa8c7326161cc49af3df0be5dfae4

Added default values for pathnames.
This allows one to simply run `./configure` and expect it to
produce sane defaults for an install.

Previously, without specifying `--prefix=...`, `make install`
would simply fail, recommending to set `--prefix` or `DESTDIR`,
but that recommendation was incomplete at best, since it didn't
set many of the subdirs needed for a good organization.

Setting `DESTDIR` was even worse, since that shouldn't even affect
an installation (it is required to be transparent to the
installation).

/usr/local is the historic Unix standard path to use for
installations from source made manually by the admin of the
system.  Some package managers (Homebrew, I'm looking specifically
at you) have abused that path to install their things, but 1) it's
not our fault that someone else incorrectly abuses that path (and
they seem to be fixing it for newer archs; e.g., they started
using /opt/homebrew for Apple Silicon), 2) there's no better path
than /usr/local, 3) we still allow changing it for systems where
this might not be the desired path (MacOS Intel with hombrew), and
4) it's _the standard_.

See a related conversation with Ingo (OpenBSD maintainer):

On 7/27/22 16:16, Ingo Schwarze wrote:
> Hi Alejandro,
[...]
>
> Alejandro Colomar wrote on Sun, Jul 24, 2022 at 07:07:18PM +0200:
>> On 7/24/22 16:57, Ingo Schwarze wrote:
>>> Alejandro Colomar wrote on Sun, Jul 24, 2022 at 01:20:46PM +0200:
>
>>>> /usr/local is for sysadmins to build from source;
>
>>> Doing that is *very* strongly discouraged on OpenBSD.
>
>> I guess that's why the directory was reused in the BSDs to install ports
>> (probably ports were installed by the sysadmin there, and by extension,
>> ports are now always installed there, but that's just a guess).
>
> Maybe.  In any case, the practice of using /usr/local for packages
> created from ports is significantly older than the recommendation
> to refrain from using upstream "make install" outside the ports
> framework.
>

src/nxt_runtime.c
@@ -768,10 +768,10 @@ nxt_runtime_conf_init(nxt_task_t *task, nxt_runtime_t *rt)
     rt->group = NXT_GROUP;
     rt->pid = NXT_PID;
     rt->log = NXT_LOG;
-    rt->modules = NXT_MODULES;
-    rt->state = NXT_STATE;
+    rt->modules = NXT_MODULESDIR;
+    rt->state = NXT_LIBSTATEDIR;
     rt->control = NXT_CONTROL_SOCK;
-    rt->tmp = NXT_TMP;
+    rt->tmp = NXT_TMPDIR;
 
     nxt_memzero(&rt->capabilities, sizeof(nxt_capabilities_t));
 
@@ -927,9 +927,10 @@ nxt_runtime_conf_read_cmd(nxt_task_t *task, nxt_runtime_t *rt)
     static const char  no_pid[] = "option \"--pid\" requires filename\n";
     static const char  no_log[] = "option \"--log\" requires filename\n";
     static const char  no_modules[] =
-                       "option \"--modules\" requires directory\n";
-    static const char  no_state[] = "option \"--state\" requires directory\n";
-    static const char  no_tmp[] = "option \"--tmp\" requires directory\n";
+                       "option \"--modulesdir\" requires directory\n";
+    static const char  no_state[] =
+                       "option \"--libstatedir\" requires directory\n";
+    static const char  no_tmp[] = "option \"--tmpdir\" requires directory\n";
 
     static const char  help[] =
         "\n"
@@ -948,14 +949,14 @@ nxt_runtime_conf_read_cmd(nxt_task_t *task, nxt_runtime_t *rt)
         "  --log FILE           set log filename\n"
         "                       default: \"" NXT_LOG "\"\n"
         "\n"
-        "  --modules DIRECTORY  set modules directory name\n"
-        "                       default: \"" NXT_MODULES "\"\n"
+        "  --modulesdir DIR     set modules directory name\n"
+        "                       default: \"" NXT_MODULESDIR "\"\n"
         "\n"
-        "  --state DIRECTORY    set state directory name\n"
-        "                       default: \"" NXT_STATE "\"\n"
+        "  --libstatedir DIR    set state directory name\n"
+        "                       default: \"" NXT_LIBSTATEDIR "\"\n"
         "\n"
-        "  --tmp DIRECTORY      set tmp directory name\n"
-        "                       default: \"" NXT_TMP "\"\n"
+        "  --tmpdir DIR         set tmp directory name\n"
+        "                       default: \"" NXT_TMPDIR "\"\n"
         "\n"
         "  --user USER          set non-privileged processes to run"
                                 " as specified user\n"
@@ -1038,7 +1039,7 @@ nxt_runtime_conf_read_cmd(nxt_task_t *task, nxt_runtime_t *rt)
             continue;
         }
 
-        if (nxt_strcmp(p, "--modules") == 0) {
+        if (nxt_strcmp(p, "--modulesdir") == 0) {
             if (*argv == NULL) {
                 write(STDERR_FILENO, no_modules, nxt_length(no_modules));
                 return NXT_ERROR;
@@ -1051,7 +1052,7 @@ nxt_runtime_conf_read_cmd(nxt_task_t *task, nxt_runtime_t *rt)
             continue;
         }
 
-        if (nxt_strcmp(p, "--state") == 0) {
+        if (nxt_strcmp(p, "--libstatedir") == 0) {
             if (*argv == NULL) {
                 write(STDERR_FILENO, no_state, nxt_length(no_state));
                 return NXT_ERROR;
@@ -1064,7 +1065,7 @@ nxt_runtime_conf_read_cmd(nxt_task_t *task, nxt_runtime_t *rt)
             continue;
         }
 
-        if (nxt_strcmp(p, "--tmp") == 0) {
+        if (nxt_strcmp(p, "--tmpdir") == 0) {
             if (*argv == NULL) {
                 write(STDERR_FILENO, no_tmp, nxt_length(no_tmp));
                 return NXT_ERROR;

auto/help
@@ -11,23 +11,31 @@ cat << END
   --cc-opt=OPTIONS     set additional C compiler options
   --ld-opt=OPTIONS     set additional linker options
 
-  --prefix=DIRECTORY   set prefix for relative pathnames, default: none
-  --bindir=DIRECTORY   set user executables directory name
-                       default: "$NXT_BINDIR"
-  --sbindir=DIRECTORY  set system admin executables directory name
-                       default: "$NXT_SBINDIR"
-  --libdir=DIRECTORY   set library directory name, default: "$NXT_LIBDIR"
-  --incdir=DIRECTORY   set includes directory name, default: "$NXT_INCDIR"
-  --mandir=DIRECTORY   set man pages directory name, default: "$NXT_MANDIR"
-  --modules=DIRECTORY  set modules directory name, default: "$NXT_MODULES"
-  --state=DIRECTORY    set state directory name, default: "$NXT_STATE"
-  --tmp=DIRECTORY      set tmp directory name, default: "$NXT_TMP"
+  --prefix=DIR         default: "/usr/local"
+  --exec-prefix=DIR    default: "\$prefix"
+  --bindir=DIR         default: "\$exec_prefix/bin"
+  --sbindir=DIR        default: "\$exec_prefix/sbin"
+  --includedir=DIR     default: "\$prefix/include"
+  --libdir=DIR         default: "\$exec_prefix/lib"
+  --modulesdir=DIR     default: "\$libdir/unit/modules"
+  --datarootdir=DIR    default: "\$prefix/share"
+  --mandir=DIR         default: "\$datarootdir/man"
+  --localstatedir=DIR  default: "\$prefix/var"
+  --libstatedir=DIR    default: "\$localstatedir/lib/unit"
+  --runstatedir=DIR    default: "\$localstatedir/run/unit"
+  --logdir=DIR         default: "\$localstatedir/log/unit"
+  --tmpdir=DIR         default: "/tmp"
 
-  --pid=FILE           set pid filename, default: "$NXT_PID"
-  --log=FILE           set log filename, default: "$NXT_LOG"
+  --incdir=DIR         [deprecated] synonym for --includedir
+  --modules=DIR        [deprecated] synonym for --modulesdir
+  --state=DIR          [deprecated] synonym for --libstatedir
+  --tmp=DIR            [deprecated] synonym for --tmpdir
+
+  --pid=FILE           set pid filename, default: "\$runstatedir/unit.pid"
+  --log=FILE           set log filename, default: "\$logdir/unit.log"
 
   --control=ADDRESS    set address of control API socket
-                       default: "$NXT_CONTROL"
+                       default: "unix:\$runstatedir/control.unit.sock"
 
   --user=USER          set non-privileged processes to run as specified user
                        default: "$NXT_USER"

auto/make
@@ -366,8 +366,12 @@ ${NXT_DAEMON}-install: $NXT_DAEMON install-check
 	test -d \$(DESTDIR)$NXT_SBINDIR \
 		|| install -d \$(DESTDIR)$NXT_SBINDIR
 	install -p $NXT_BUILD_DIR/$NXT_DAEMON \$(DESTDIR)$NXT_SBINDIR/
-	test -d \$(DESTDIR)$NXT_STATE \
-		|| install -d \$(DESTDIR)$NXT_STATE
+	test -d \$(DESTDIR)$NXT_LIBSTATEDIR \
+		|| install -d \$(DESTDIR)$NXT_LIBSTATEDIR
+	test -d \$(DESTDIR)$NXT_LOGDIR \
+		|| install -d \$(DESTDIR)$NXT_LOGDIR
+	test -d \$(DESTDIR)$NXT_RUNSTATEDIR \
+		|| install -d \$(DESTDIR)$NXT_RUNSTATEDIR
 
 manpage-install: manpage install-check
 	test -d \$(DESTDIR)$NXT_MANDIR/man8 \
@@ -397,8 +401,8 @@ libunit-install: $NXT_BUILD_DIR/$NXT_LIB_UNIT_STATIC
 		|| install -d \$(DESTDIR)$NXT_LIBDIR
 	install -p -m u=rw,go=r $NXT_BUILD_DIR/$NXT_LIB_UNIT_STATIC \
 		\$(DESTDIR)$NXT_LIBDIR/
-	test -d \$(DESTDIR)$NXT_INCDIR \
-		|| install -d \$(DESTDIR)$NXT_INCDIR
+	test -d \$(DESTDIR)$NXT_INCLUDEDIR \
+		|| install -d \$(DESTDIR)$NXT_INCLUDEDIR
 	install -p -m u=rw,go=r src/nxt_unit.h \
 		src/nxt_unit_field.h \
 		src/nxt_unit_request.h \
@@ -409,22 +413,22 @@ libunit-install: $NXT_BUILD_DIR/$NXT_LIB_UNIT_STATIC
 		$NXT_BUILD_DIR/nxt_auto_config.h \
 		$NXT_BUILD_DIR/nxt_version.h \
 		src/nxt_websocket_header.h \
-		\$(DESTDIR)$NXT_INCDIR/
+		\$(DESTDIR)$NXT_INCLUDEDIR/
 
 libunit-uninstall:
 	rm -f \$(DESTDIR)$NXT_LIBDIR/$NXT_LIB_UNIT_STATIC
 	@rmdir -p \$(DESTDIR)$NXT_LIBDIR 2>/dev/null || true
-	rm -f \$(DESTDIR)$NXT_INCDIR/nxt_unit.h \
-		\$(DESTDIR)$NXT_INCDIR/nxt_unit_field.h \
-		\$(DESTDIR)$NXT_INCDIR/nxt_unit_request.h \
-		\$(DESTDIR)$NXT_INCDIR/nxt_unit_response.h \
-		\$(DESTDIR)$NXT_INCDIR/nxt_unit_sptr.h \
-		\$(DESTDIR)$NXT_INCDIR/nxt_unit_typedefs.h \
-		\$(DESTDIR)$NXT_INCDIR/nxt_unit_websocket.h \
-		\$(DESTDIR)$NXT_INCDIR/nxt_auto_config.h \
-		\$(DESTDIR)$NXT_INCDIR/nxt_version.h \
-		\$(DESTDIR)$NXT_INCDIR/nxt_websocket_header.h
-	@rmdir -p \$(DESTDIR)$NXT_INCDIR 2>/dev/null || true
+	rm -f \$(DESTDIR)$NXT_INCLUDEDIR/nxt_unit.h \
+		\$(DESTDIR)$NXT_INCLUDEDIR/nxt_unit_field.h \
+		\$(DESTDIR)$NXT_INCLUDEDIR/nxt_unit_request.h \
+		\$(DESTDIR)$NXT_INCLUDEDIR/nxt_unit_response.h \
+		\$(DESTDIR)$NXT_INCLUDEDIR/nxt_unit_sptr.h \
+		\$(DESTDIR)$NXT_INCLUDEDIR/nxt_unit_typedefs.h \
+		\$(DESTDIR)$NXT_INCLUDEDIR/nxt_unit_websocket.h \
+		\$(DESTDIR)$NXT_INCLUDEDIR/nxt_auto_config.h \
+		\$(DESTDIR)$NXT_INCLUDEDIR/nxt_version.h \
+		\$(DESTDIR)$NXT_INCLUDEDIR/nxt_websocket_header.h
+	@rmdir -p \$(DESTDIR)$NXT_INCLUDEDIR 2>/dev/null || true
 
 END
 

auto/modules/java
@@ -63,7 +63,7 @@ fi
 . $NXT_AUTOCONF_DATA
 
 
-NXT_JARS=${NXT_JARS=$NXT_MODULES}
+NXT_JARS=${NXT_JARS=$NXT_MODULESDIR}
 NXT_JAVA_MODULE=${NXT_JAVA_MODULE=java}
 NXT_JAVA_LIB_PATH=${NXT_JAVA_LIB_PATH=}
 
@@ -535,9 +535,9 @@ ${NXT_JAVA_MODULE}-install: $NXT_BUILD_DIR/${NXT_JAVA_MODULE}.unit.so \\
     $NXT_BUILD_DIR/$NXT_UNIT_JAR \\
     $NXT_BUILD_DIR/$NXT_WS_API_JAR \\
     java-shared-install
-	install -d \$(DESTDIR)$NXT_MODULES
+	install -d \$(DESTDIR)$NXT_MODULESDIR
 	install -p $NXT_BUILD_DIR/${NXT_JAVA_MODULE}.unit.so \\
-		\$(DESTDIR)$NXT_MODULES/
+		\$(DESTDIR)$NXT_MODULESDIR/
 	install -d \$(DESTDIR)$NXT_JARS
 	install -p -m 0644 $NXT_BUILD_DIR/$NXT_UNIT_JAR \$(DESTDIR)$NXT_JARS/
 	install -p -m 0644 $NXT_BUILD_DIR/$NXT_WS_API_JAR \$(DESTDIR)$NXT_JARS/
@@ -546,8 +546,8 @@ ${NXT_JAVA_MODULE}-install: $NXT_BUILD_DIR/${NXT_JAVA_MODULE}.unit.so \\
 uninstall: ${NXT_JAVA_MODULE}-uninstall
 
 ${NXT_JAVA_MODULE}-uninstall: java-shared-uninstall
-	rm -f \$(DESTDIR)$NXT_MODULES/${NXT_JAVA_MODULE}.unit.so
-	@rmdir -p \$(DESTDIR)$NXT_MODULES 2>/dev/null || true
+	rm -f \$(DESTDIR)$NXT_MODULESDIR/${NXT_JAVA_MODULE}.unit.so
+	@rmdir -p \$(DESTDIR)$NXT_MODULESDIR 2>/dev/null || true
 	rm -f \$(DESTDIR)$NXT_JARS/$NXT_UNIT_JAR
 	rm -f \$(DESTDIR)$NXT_JARS/$NXT_WS_API_JAR
 	@rmdir -p \$(DESTDIR)$NXT_JARS 2>/dev/null || true

auto/modules/perl
@@ -195,15 +195,15 @@ $NXT_BUILD_DIR/${NXT_PERL_MODULE}.unit.so:	$nxt_objs
 install: ${NXT_PERL_MODULE}-install
 
 ${NXT_PERL_MODULE}-install: ${NXT_PERL_MODULE} install-check
-	install -d \$(DESTDIR)$NXT_MODULES
+	install -d \$(DESTDIR)$NXT_MODULESDIR
 	install -p $NXT_BUILD_DIR/${NXT_PERL_MODULE}.unit.so \\
-		\$(DESTDIR)$NXT_MODULES/
+		\$(DESTDIR)$NXT_MODULESDIR/
 
 
 uninstall: ${NXT_PERL_MODULE}-uninstall
 
 ${NXT_PERL_MODULE}-uninstall:
-	rm -f \$(DESTDIR)$NXT_MODULES/${NXT_PERL_MODULE}.unit.so
-	@rmdir -p \$(DESTDIR)$NXT_MODULES 2>/dev/null || true
+	rm -f \$(DESTDIR)$NXT_MODULESDIR/${NXT_PERL_MODULE}.unit.so
+	@rmdir -p \$(DESTDIR)$NXT_MODULESDIR 2>/dev/null || true
 
 END

auto/modules/php
@@ -275,15 +275,15 @@ $NXT_BUILD_DIR/${NXT_PHP_MODULE}.unit.so:	$nxt_objs
 install: ${NXT_PHP_MODULE}-install
 
 ${NXT_PHP_MODULE}-install: ${NXT_PHP_MODULE} install-check
-	install -d \$(DESTDIR)$NXT_MODULES
+	install -d \$(DESTDIR)$NXT_MODULESDIR
 	install -p $NXT_BUILD_DIR/${NXT_PHP_MODULE}.unit.so \\
-		\$(DESTDIR)$NXT_MODULES/
+		\$(DESTDIR)$NXT_MODULESDIR/
 
 
 uninstall: ${NXT_PHP_MODULE}-uninstall
 
 ${NXT_PHP_MODULE}-uninstall:
-	rm -f \$(DESTDIR)$NXT_MODULES/${NXT_PHP_MODULE}.unit.so
-	@rmdir -p \$(DESTDIR)$NXT_MODULES 2>/dev/null || true
+	rm -f \$(DESTDIR)$NXT_MODULESDIR/${NXT_PHP_MODULE}.unit.so
+	@rmdir -p \$(DESTDIR)$NXT_MODULESDIR 2>/dev/null || true
 
 END

auto/modules/python
@@ -223,15 +223,15 @@ $NXT_BUILD_DIR/${NXT_PYTHON_MODULE}.unit.so:	$nxt_objs
 install: ${NXT_PYTHON_MODULE}-install
 
 ${NXT_PYTHON_MODULE}-install: ${NXT_PYTHON_MODULE} install-check
-	install -d \$(DESTDIR)$NXT_MODULES
+	install -d \$(DESTDIR)$NXT_MODULESDIR
 	install -p $NXT_BUILD_DIR/${NXT_PYTHON_MODULE}.unit.so \\
-		\$(DESTDIR)$NXT_MODULES/
+		\$(DESTDIR)$NXT_MODULESDIR/
 
 
 uninstall: ${NXT_PYTHON_MODULE}-uninstall
 
 ${NXT_PYTHON_MODULE}-uninstall:
-	rm -f \$(DESTDIR)$NXT_MODULES/${NXT_PYTHON_MODULE}.unit.so
-	@rmdir -p \$(DESTDIR)$NXT_MODULES 2>/dev/null || true
+	rm -f \$(DESTDIR)$NXT_MODULESDIR/${NXT_PYTHON_MODULE}.unit.so
+	@rmdir -p \$(DESTDIR)$NXT_MODULESDIR 2>/dev/null || true
 
 END

auto/modules/ruby
@@ -261,15 +261,15 @@ $NXT_BUILD_DIR/${NXT_RUBY_MODULE}.unit.so:	$nxt_objs
 install: ${NXT_RUBY_MODULE}-install
 
 ${NXT_RUBY_MODULE}-install: ${NXT_RUBY_MODULE} install-check
-	install -d \$(DESTDIR)$NXT_MODULES
+	install -d \$(DESTDIR)$NXT_MODULESDIR
 	install -p $NXT_BUILD_DIR/${NXT_RUBY_MODULE}.unit.so \\
-		\$(DESTDIR)$NXT_MODULES/
+		\$(DESTDIR)$NXT_MODULESDIR/
 
 
 uninstall: ${NXT_RUBY_MODULE}-uninstall
 
 ${NXT_RUBY_MODULE}-uninstall:
-	rm -f \$(DESTDIR)$NXT_MODULES/${NXT_RUBY_MODULE}.unit.so
-	@rmdir -p \$(DESTDIR)$NXT_MODULES 2>/dev/null || true
+	rm -f \$(DESTDIR)$NXT_MODULESDIR/${NXT_RUBY_MODULE}.unit.so
+	@rmdir -p \$(DESTDIR)$NXT_MODULESDIR 2>/dev/null || true
 
 END

commit f32858dcb76a35513c035729d1c44ce01520e84c

Added back deprecated options to unitd.
We renamed the options recently, with the intention of keeping the old
names as supported but deprecated for some time, before removal.  This
was done with the configure script options, but in the unitd binary, we
accidentally removed the old names, causing some unintended breakage.
Keep support for the old names, albeit with a deprecation message to
stderr, for some time, until we decide to remove them.

Fixes: 5a37171f733f ("Added default values for pathnames.")
Closes: <https://github.com/nginx/unit/issues/876>
Reported-by: El RIDO <elrido@gmx.net>
Acked-by: Liam Crilly <liam@nginx.com>
Acked-by: Artem Konev <a.konev@f5.com>
Acked-by: Timo Stark <t.stark@nginx.com>
Reviewed-by: Andrew Clayton <a.clayton@nginx.com>
Cc: Andrei Zeliankou <zelenkov@nginx.com>
Signed-off-by: Alejandro Colomar <alx@nginx.com>

src/nxt_runtime.c
@@ -966,6 +966,13 @@ nxt_runtime_conf_read_cmd(nxt_task_t *task, nxt_runtime_t *rt)
                        "option \"--statedir\" requires directory\n";
     static const char  no_tmp[] = "option \"--tmpdir\" requires directory\n";
 
+    static const char  modules_deprecated[] =
+           "option \"--modules\" is deprecated; use \"--modulesdir\" instead\n";
+    static const char  state_deprecated[] =
+           "option \"--state\" is deprecated; use \"--statedir\" instead\n";
+    static const char  tmp_deprecated[] =
+           "option \"--tmp\" is deprecated; use \"--tmpdir\" instead\n";
+
     static const char  help[] =
         "\n"
         "unit options:\n"
@@ -991,6 +998,10 @@ nxt_runtime_conf_read_cmd(nxt_task_t *task, nxt_runtime_t *rt)
         "\n"
         "  --tmpdir DIR         set tmp directory name\n"
         "                       default: \"" NXT_TMPDIR "\"\n"
+        "\n"
+        "  --modules DIR        [deprecated] synonym for --modulesdir\n"
+        "  --state DIR          [deprecated] synonym for --statedir\n"
+        "  --tmp DIR            [deprecated] synonym for --tmpdir\n"
         "\n"
         "  --user USER          set non-privileged processes to run"
                                 " as specified user\n"
@@ -1073,7 +1084,14 @@ nxt_runtime_conf_read_cmd(nxt_task_t *task, nxt_runtime_t *rt)
             continue;
         }
 
+        if (nxt_strcmp(p, "--modules") == 0) {
+            write(STDERR_FILENO, modules_deprecated,
+                  nxt_length(modules_deprecated));
+            goto modulesdir;
+        }
+
         if (nxt_strcmp(p, "--modulesdir") == 0) {
+modulesdir:
             if (*argv == NULL) {
                 write(STDERR_FILENO, no_modules, nxt_length(no_modules));
                 return NXT_ERROR;
@@ -1086,7 +1104,14 @@ nxt_runtime_conf_read_cmd(nxt_task_t *task, nxt_runtime_t *rt)
             continue;
         }
 
+        if (nxt_strcmp(p, "--state") == 0) {
+            write(STDERR_FILENO, state_deprecated,
+                  nxt_length(state_deprecated));
+            goto statedir;
+        }
+
         if (nxt_strcmp(p, "--statedir") == 0) {
+statedir:
             if (*argv == NULL) {
                 write(STDERR_FILENO, no_state, nxt_length(no_state));
                 return NXT_ERROR;
@@ -1099,7 +1124,13 @@ nxt_runtime_conf_read_cmd(nxt_task_t *task, nxt_runtime_t *rt)
             continue;
         }
 
+        if (nxt_strcmp(p, "--tmp") == 0) {
+            write(STDERR_FILENO, tmp_deprecated, nxt_length(tmp_deprecated));
+            goto tmpdir;
+        }
+
         if (nxt_strcmp(p, "--tmpdir") == 0) {
+tmpdir:
             if (*argv == NULL) {
                 write(STDERR_FILENO, no_tmp, nxt_length(no_tmp));
                 return NXT_ERROR;