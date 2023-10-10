d1a9069852b78e43aa4e0265cf193743019f41be
Sergey A. Osokin <sergey.osokin@nginx.com>
2023-10-10 11:55:44 -0400

    Update third-party components for the Java module.

--- test/unit/applications/lang/java.py
+++ test/unit/applications/lang/java.py
@@ -53,7 +53,7 @@ class ApplicationJava(ApplicationProto):
                 os.makedirs(classes_path)
 
             classpath = (
-                f'{option.current_dir}/build/tomcat-servlet-api-9.0.80.jar'
+                f'{option.current_dir}/build/tomcat-servlet-api-9.0.81.jar'
             )
 
             ws_jars = glob.glob(
