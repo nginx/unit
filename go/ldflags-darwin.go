nginx/unit

commit 9a36de84c8534cdd8d22b33d90309519e03abf3b

Go: Use Homebrew include paths
Fixes nginx/unit#967

go/ldflags-darwin.go
@@ -1,0 +1,14 @@
+//go:build darwin
+
+/*
+ * Copyright (C) Danielle De Leo
+ * Copyright (C) NGINX, Inc.
+ */
+
+package unit
+
+/*
+#cgo LDFLAGS: -L/opt/homebrew/lib
+#cgo CFLAGS: -I/opt/homebrew/include
+*/
+import "C"

commit 690fd69205bfda866d3d2c37da554f520add4d8d

Go: Add missing +build and go:build comments
A RHEL 8 test was failing because it uses go1.16. The old style must
be retained for backwards compat.

Fixes: 9a36de84c ("Go: Use Homebrew include paths")
Reviewed-by: Andrew Clayton <a.clayton@nginx.com>
Reviewed-by: Dylan Arbour <d.arbour@f5.com>
Signed-off-by: Danielle De Leo <d.deleo@f5.com>

go/ldflags-darwin.go
@@ -1,4 +1,5 @@
 //go:build darwin
+// +build darwin
 
 /*
  * Copyright (C) Danielle De Leo

go/ldflags-lrt.go
@@ -1,3 +1,4 @@
+//go:build linux || netbsd
 // +build linux netbsd
 
 /*