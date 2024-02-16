3aec4bd51d8a0d8a463d358384dc1750967cd8aa
Andrei Zeliankou <zelenkov@nginx.com>
2024-02-16 18:22:35 +0000

    NJS: loader should be registered using njs_vm_set_module_loader()
    This change makes NJS module incompatible with NJS older than 0.8.3.
    Therefore, the configuration version check has been adjusted accordingly.
    
    This change was introduced in NJS 0.8.3 here:
    <https://hg.nginx.com/njs/rev/ad1a7ad3c715>

--- src/nxt_js.c
+++ src/nxt_js.c
@@ -69,14 +69,6 @@ nxt_js_module_loader(njs_vm_t *vm, njs_external_ptr_t external, njs_str_t *name)
 }
 
 
-static njs_vm_ops_t  nxt_js_ops = {
-    NULL,
-    NULL,
-    nxt_js_module_loader,
-    NULL,
-};
-
-
 njs_int_t  nxt_js_proto_id;
 
 
@@ -127,6 +119,7 @@ nxt_js_vm_create(nxt_js_conf_t *jcf)
 {
     u_char           *p;
     size_t           size;
+    njs_vm_t         *vm;
     nxt_uint_t       i;
     njs_vm_opt_t     opts;
     nxt_js_module_t  *module, *mod;
@@ -146,7 +139,6 @@ nxt_js_vm_create(nxt_js_conf_t *jcf)
         goto done;
     }
 
-    opts.ops = &nxt_js_ops;
     opts.external = jcf;
 
     size = 0;
@@ -203,7 +195,13 @@ nxt_js_vm_create(nxt_js_conf_t *jcf)
 
 done:
 
-    return njs_vm_create(&opts);
+    vm = njs_vm_create(&opts);
+
+    if (nxt_fast_path(vm != NULL)) {
+        njs_vm_set_module_loader(vm, nxt_js_module_loader, jcf);
+    }
+
+    return vm;
 }
 
 
