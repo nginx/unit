nginx/unit

commit 57fc9201cb91e3d8901a64e3daaaf31684ee5bf5

Socket: Created control socket & pid file directories.
@alejandro-colomar reported an issue on GitHub whereby Unit would fail
to start due to not being able to create the control socket (a Unix
Domain Socket)

  2022/08/05 20:12:22 [alert] 21613#21613 bind(6,
  unix:/opt/local/unit/var/run/unit/control.unit.sock.tmp)
  failed (2: No such file or directory)

This could happen if the control socket was set to a directory that
doesn't exist. A common place to put the control socket would be under
/run/unit, and while /run will exist, /run/unit may well not (/run
is/should be cleared on each boot).

The pid file would also generally go under /run/unit, though this is
created after the control socket, however it could go someplace else so
we should also ensure its directory exists.

This commit will try to create the pid file and control sockets parent
directory. In some cases the user will need to ensure that the rest of
the path already exists.

This adds a new nxt_fs_mkdir_parent() function that given a full path
to a file (or directory), strips the last component off before passing
the remaining directory path to nxt_fs_mkdir().

Cc: Konstantin Pavlov <thresh@nginx.com>
Closes: <https://github.com/nginx/unit/issues/742>
Reported-by: Alejandro Colomar <alx@nginx.com>
Reviewed-by: Alejandro Colomar <alx@nginx.com>
Tested-by: Alejandro Colomar <alx@nginx.com>
Signed-off-by: Andrew Clayton <a.clayton@nginx.com>

src/nxt_controller.c
@@ -666,6 +666,14 @@ nxt_runtime_controller_socket(nxt_task_t *task, nxt_runtime_t *rt)
 #endif
     ls->handler = nxt_controller_conn_init;
 
+#if (NXT_HAVE_UNIX_DOMAIN)
+    if (ls->sockaddr->u.sockaddr.sa_family == AF_UNIX) {
+        const char *path = ls->sockaddr->u.sockaddr_un.sun_path;
+
+        nxt_fs_mkdir_parent((const u_char *) path, 0755);
+    }
+#endif
+
     if (nxt_listen_socket_create(task, rt->mem_pool, ls) != NXT_OK) {
         return NXT_ERROR;
     }

src/nxt_fs.c
@@ -273,6 +273,31 @@ nxt_fs_mkdir_all(const u_char *dir, mode_t mode)
 }
 
 
+nxt_int_t
+nxt_fs_mkdir_parent(const u_char *path, mode_t mode)
+{
+    char       *ptr, *dir;
+    nxt_int_t  ret;
+
+    dir = nxt_strdup(path);
+    if (nxt_slow_path(dir == NULL)) {
+        return NXT_ERROR;
+    }
+
+    ret = NXT_OK;
+
+    ptr = strrchr(dir, '/');
+    if (nxt_fast_path(ptr != NULL)) {
+        *ptr = '\0';
+        ret = nxt_fs_mkdir((const u_char *) dir, mode);
+    }
+
+    nxt_free(dir);
+
+    return ret;
+}
+
+
 static nxt_int_t
 nxt_fs_mkdir(const u_char *dir, mode_t mode)
 {

src/nxt_fs.h
@@ -36,6 +36,7 @@ typedef struct {
 } nxt_fs_mount_t;
 
 
+nxt_int_t nxt_fs_mkdir_parent(const u_char *path, mode_t mode);
 nxt_int_t nxt_fs_mkdir_all(const u_char *dir, mode_t mode);
 nxt_int_t nxt_fs_mount(nxt_task_t *task, nxt_fs_mount_t *mnt);
 void nxt_fs_unmount(const u_char *path);

src/nxt_runtime.c
@@ -1369,6 +1369,8 @@ nxt_runtime_pid_file_create(nxt_task_t *task, nxt_file_name_t *pid_file)
 
     file.name = pid_file;
 
+    nxt_fs_mkdir_parent(pid_file, 0755);
+
     n = nxt_file_open(task, &file, O_WRONLY, O_CREAT | O_TRUNC,
                       NXT_FILE_DEFAULT_ACCESS);
 

commit 4ed9df7c6d003e80ceb692d4b7faece2057e1715

Fs: Make parents recursively for the pid file and the control socket
Build systems should not attempt to create $runstatedir (or anything
under it).  Doing so causes warnings in packaging systems, such as in
Alpine Linux, as reported by Andy.

But unitd(8) can be configured to be installed under /opt, or other
trees, where no directories exist before hand.  Expecting that the user
creates the entire directory trees that unit will need is a bit
unreasonable.  Instead, we can just create any directories that we need,
with all their parents.

Fixes: 57fc9201cb91 ("Socket: Created control socket & pid file directories.")
Link: <https://github.com/nginx/unit/issues/742>
Reported-by: Andy Postnikov <apostnikov@gmail.com>
Cc: Andrew Clayton <a.clayton@nginx.com>
Cc: Liam Crilly <liam@nginx.com>
Cc: Konstantin Pavlov <thresh@nginx.com>
Signed-off-by: Alejandro Colomar <alx@kernel.org>

src/nxt_controller.c
@@ -695,7 +695,7 @@ nxt_runtime_controller_socket(nxt_task_t *task, nxt_runtime_t *rt)
     if (ls->sockaddr->u.sockaddr.sa_family == AF_UNIX) {
         const char *path = ls->sockaddr->u.sockaddr_un.sun_path;
 
-        nxt_fs_mkdir_parent((const u_char *) path, 0755);
+        nxt_fs_mkdir_parents_dirname((const u_char *) path, 0755);
     }
 #endif
 

src/nxt_fs.c
@@ -5,6 +5,8 @@
 
 #include <nxt_main.h>
 
+#include <errno.h>
+
 
 static nxt_int_t nxt_fs_mkdir(const u_char *dir, mode_t mode);
 
@@ -50,7 +52,7 @@ nxt_fs_mkdir_all(const u_char *dir, mode_t mode)
 
 
 nxt_int_t
-nxt_fs_mkdir_parent(const u_char *path, mode_t mode)
+nxt_fs_mkdir_parents_dirname(const u_char *path, mode_t mode)
 {
     char       *ptr, *dir;
     nxt_int_t  ret;
@@ -63,12 +65,23 @@ nxt_fs_mkdir_parent(const u_char *path, mode_t mode)
     ret = NXT_OK;
 
     ptr = strrchr(dir, '/');
-    if (nxt_slow_path(ptr == NULL || ptr == dir)) {
+    if (ptr == dir || nxt_slow_path(ptr == NULL)) {
         goto out_free;
     }
 
     *ptr = '\0';
+    if (strcmp(dir, (const char *) path) != 0)
+    {
+        ret = nxt_fs_mkdir_parents_dirname((const u_char *) dir, mode);
+        if (nxt_slow_path(ret == NXT_ERROR)) {
+            goto out_free;
+        }
+    }
+
     ret = nxt_fs_mkdir((const u_char *) dir, mode);
+    if (ret == NXT_ERROR && errno == EEXIST) {
+        ret = NXT_OK;
+    }
 
 out_free:
     nxt_free(dir);

src/nxt_fs.h
@@ -6,7 +6,7 @@
 #define _NXT_FS_H_INCLUDED_
 
 
-nxt_int_t nxt_fs_mkdir_parent(const u_char *path, mode_t mode);
+nxt_int_t nxt_fs_mkdir_parents_dirname(const u_char *path, mode_t mode);
 nxt_int_t nxt_fs_mkdir_all(const u_char *dir, mode_t mode);
 
 

src/nxt_runtime.c
@@ -1490,7 +1490,7 @@ nxt_runtime_pid_file_create(nxt_task_t *task, nxt_file_name_t *pid_file)
 
     file.name = pid_file;
 
-    nxt_fs_mkdir_parent(pid_file, 0755);
+    nxt_fs_mkdir_parents_dirname(pid_file, 0755);
 
     n = nxt_file_open(task, &file, O_WRONLY, O_CREAT | O_TRUNC,
                       NXT_FILE_DEFAULT_ACCESS);