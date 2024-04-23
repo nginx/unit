351573eefa80be197c40ae759bc1c6044cefa868
Alejandro Colomar <alx@kernel.org>
2024-04-23 23:27:06 +0200

    fs: Accept path names of length 1
    That is, accept "/", or relative path names of a single byte.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/nxt_fs.c
+++ src/nxt_fs.c
@@ -17,7 +17,7 @@ nxt_fs_mkdir_all(const u_char *dir, mode_t mode)
 
     dirlen = nxt_strlen(dir);
 
-    nxt_assert(dirlen < PATH_MAX && dirlen > 1);
+    nxt_assert(dirlen < PATH_MAX && dirlen > 0);
 
     dst = path;
     start = (char *) dir;
