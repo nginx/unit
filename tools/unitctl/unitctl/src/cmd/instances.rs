818d4ad76592c87a5c0c7bbd728636023c07daa0
Ava Hahn <a.hahn@f5.com>
2024-05-01 13:59:33 -0700

    tools/unitctl: API Plumbing for docker deployments
    * refactored "instance" command out of enum
    * plumbed through function stub from client library
    * error handling
    
    Signed-off-by: Ava Hahn <a.hahn@f5.com>

--- tools/unitctl/unitctl/src/cmd/instances.rs
+++ tools/unitctl/unitctl/src/cmd/instances.rs
@@ -1,16 +1,37 @@
 use crate::{OutputFormat, UnitctlError};
+use crate::unitctl::{InstanceArgs, InstanceCommands};
 use unit_client_rs::unitd_instance::UnitdInstance;
+use unit_client_rs::unitd_docker::deploy_new_container;
+use std::path::PathBuf;
 
-pub(crate) async fn cmd(output_format: OutputFormat) -> Result<(), UnitctlError> {
-    let instances = UnitdInstance::running_unitd_instances().await;
-    if instances.is_empty() {
-        Err(UnitctlError::NoUnitInstancesError)
-    } else if output_format.eq(&OutputFormat::Text) {
-        instances.iter().for_each(|instance| {
-            println!("{}", instance);
-        });
-        Ok(())
+pub(crate) async fn cmd(args: InstanceArgs) -> Result<(), UnitctlError> {
+    if let Some(cmd) = args.command {
+        match cmd {
+            InstanceCommands::New{
+                ref socket,
+                ref application,
+                ref image
+            } => {
+                if !PathBuf::from(socket).is_dir() || !PathBuf::from(application).is_dir() {
+                    eprintln!("application and socket paths must be directories");
+                    Err(UnitctlError::NoFilesImported)
+                } else {
+                    deploy_new_container(socket, application, image)
+                        .or_else(|e| Err(UnitctlError::UnitClientError{source: e}))
+                }
+            }
+        }
     } else {
-        output_format.write_to_stdout(&instances)
+        let instances = UnitdInstance::running_unitd_instances().await;
+        if instances.is_empty() {
+            Err(UnitctlError::NoUnitInstancesError)
+        } else if args.output_format.eq(&OutputFormat::Text) {
+            instances.iter().for_each(|instance| {
+                println!("{}", instance);
+            });
+            Ok(())
+        } else {
+            args.output_format.write_to_stdout(&instances)
+        }
     }
 }
--- tools/unitctl/unitctl/src/main.rs
+++ tools/unitctl/unitctl/src/main.rs
@@ -28,11 +28,11 @@ async fn main() -> Result<(), UnitctlError> {
     let cli = UnitCtl::parse();
 
     match cli.command {
-        Commands::Instances { output_format } => instances::cmd(output_format).await,
+        Commands::Instances(args) => instances::cmd(args).await,
 
-        Commands::Edit { output_format } => edit::cmd(&cli, output_format).await,
+        Commands::Edit{output_format} => edit::cmd(&cli, output_format).await,
 
-        Commands::Import { ref directory } => import::cmd(&cli, directory).await,
+        Commands::Import{ref directory} => import::cmd(&cli, directory).await,
 
         Commands::Execute {
             ref output_format,
@@ -41,9 +41,9 @@ async fn main() -> Result<(), UnitctlError> {
             ref path,
         } => execute_cmd::cmd(&cli, output_format, input_file, method, path).await,
 
-        Commands::Status { output_format } => status::cmd(&cli, output_format).await,
+        Commands::Status{output_format} => status::cmd(&cli, output_format).await,
 
-        Commands::Listeners { output_format } => listeners::cmd(&cli, output_format).await,
+        Commands::Listeners{output_format}=> listeners::cmd(&cli, output_format).await,
     }
     .map_err(|error| {
         eprint_error(&error);
